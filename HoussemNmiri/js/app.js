2687193871fb0240f490b15f737083eeda5b295e
NmiriHoussem <houssem.addin@gmail.com>
2018-05-11 22:05:25 +0200

    app

---
+++ HoussemNmiri/js/app.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#app',
+    data: {
+        name: 'Houssem Nmiri',
+        link: 'https://www.houssemnmiri.com',
+        description: 'Easily get in touch with Houssem Nmiri and recieve Special offers & Updates.'
+    }
+})
